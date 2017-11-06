261f4e39440a630f88e911bd2fb57303ca7790e8
hnsCreations <harin.ncsl@gmail.com>
2017-11-07 01:14:27 +0530

    7/11/2

--- admin/application/views/s_login.php
+++ admin/application/views/s_login.php
@@ -11,12 +11,12 @@
 	<body>
 		<div align="center"; class="div2"; style="margin-top:10%;"><img src="<?php echo base_url()?>designs/icon/emr.png" alt="icon" width="70px" height="70px"></div>		
 
-		<form>
+		<form method="post" action="">
 			<div align="center"; class="div2"><h4 class="h2-1">Super Admin Login</h4></div>
 
-			<div class="div3" align="center";><input type="text" name="nic" placeholder="NIC"  class="in1"><br/></div>
+			<div class="div3" align="center";><input type="text" name="nic" placeholder="Username"  class="in1"><br/></div>
 			<div class="div3" align="center";><input type="password" name="password" placeholder="Password"  class="in1"></div>
-			<div class="div3" align="center";><input type="password" name="pin" placeholder="PIN"  class="in1"></div>
+			<div class="div3" align="center";><input type="password" name="pin" placeholder="Security code"  class="in1"></div>
 					
 			<div class="div3" align="center";><button type="submit" name="register" class="button button1" style="width: 15%">Login</button></div>
 		</form>
